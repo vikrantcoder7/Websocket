06cb01cce694148ca1dff6dd39f0490929b4bfb3
vikrant sarwade <vikrantsarwade11@gmail.com>
2024-12-04 19:24:38 +0530

    add socket.io logic, simple chat app

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const http = require("http");
 const express = require("express");
 const path = require("path");
 const app = express();
+const {Server} = require("socket.io");
 
 const server = http.createServer(app);
 
+const io = new Server(server);
+
+// socket.io
+
+io.on("connection", (socket)=>{
+     socket.on("user-message", (message) =>{
+           io.emit('message', message)
+            
+     })
+     
+})
 
 app.use(express.static(path.resolve('./public')));
 
