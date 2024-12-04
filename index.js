25096d6add6695ba8b0a1d1dde548b22200a3d31
vikrant sarwade <vikrantsarwade11@gmail.com>
2024-12-04 14:48:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const server = http.createServer(app);
+
+
+app.use(express.static(path.resolve('./public')));
+
+app.get("/", (req,res) =>{
+    return  res.sendFile("./public/index.html");
+})
+
+server.listen(9000, ()=> console.log(`Server Started at PORT:9000`));
+
+
+
+
+ 
